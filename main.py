443bdd2711dbcbac299805a4e0631228d45c3da5
rbfisher15 <rbfisher@crimson.ua.edu>
2024-07-21 22:06:57 -0500

    Establish score and cavas within window

--- main.py
+++ main.py
@@ -35,6 +35,25 @@ window.resizable(False, False)
 score = 0
 direction = 'down'
 
-label = Label(window, text='Score:{}'.format(score))
+label = Label(window, text='Score:{}'.format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
